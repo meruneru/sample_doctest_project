29117a0d8838fd8d08c934d4e77d148d983e1951
M.Yamada <meruneru1032@gmail.com>
2020-06-20 17:18:52 +0900

    テスト駆動開発 8章

--- test/test.cpp
+++ test/test.cpp
@@ -4,52 +4,52 @@
 #include "../Franc.h"
 
 TEST_CASE("multiple Dollars") {
-    Dollar d(5);
-    CHECK_EQ(Dollar(5), *d.times(1));
-    CHECK_EQ(Dollar(10), *d.times(2));
+    std::unique_ptr<Money> five = Money::dollar(5);
+    CHECK_EQ(Dollar(5), *five->times(1));
+    CHECK_EQ(Dollar(10),*five->times(2));
 }
 TEST_CASE("multiple Franc") {
-    Franc f(5);
-    CHECK_EQ(Franc(5), *f.times(1));
-    CHECK_EQ(Franc(10), *f.times(2));
+    std::unique_ptr<Money> five = Money::franc(5);
+    CHECK_EQ(Franc(5), *five->times(1));
+    CHECK_EQ(Franc(10), *five->times(2));
 }
 
 TEST_CASE("equals Dollars") {
-    Dollar d1(5);
-    CHECK(d1.equals(Dollar(5)) == true);
-    CHECK(d1.equals(Dollar(50)) == false);
+    std::unique_ptr<Money> five = Money::dollar(5);
+    CHECK(five->equals(Dollar(5)) == true);
+    CHECK(five->equals(Dollar(50)) == false);
     
-    Dollar d2(0);
-    CHECK(d2.equals(Dollar(0)) == true);
-    CHECK(d2.equals(Dollar(1)) == false);
+    std::unique_ptr<Money> zero = Money::dollar(0);
+    CHECK(zero->equals(Dollar(0)) == true);
+    CHECK(zero->equals(Dollar(1)) == false);
 }
 TEST_CASE("equals operator Dollars") {
-    Dollar d1(5);
-    Dollar d2(5);
-    CHECK_EQ(Dollar(5), d1);
-    CHECK_NE(Dollar(0), d1);
+    std::unique_ptr<Money> five1 = Money::dollar(5);
+    std::unique_ptr<Money> five2 = Money::dollar(5);
+    CHECK_EQ(Dollar(5), *five1);
+    CHECK_NE(Dollar(0), *five1);
     
-    CHECK_EQ(d1, d1);
-    CHECK_EQ(d1, d2);
-    CHECK_EQ(d2, d2);
-    CHECK(d1==d2);
+    CHECK_EQ(*five1, *five1);
+    CHECK_EQ(*five2, *five2);
+    CHECK_EQ(*five1, *five2);
+    CHECK(*five1==*five2);
 }
 TEST_CASE("equals operator Franc") {
-    Franc d1(5);
-    Franc d2(5);
-    CHECK_EQ(Franc(5), d1);
-    CHECK_NE(Franc(0), d1);
+    std::unique_ptr<Money> five1 = Money::franc(5);
+    std::unique_ptr<Money> five2 = Money::franc(5);
+    CHECK_EQ(Franc(5), *five1);
+    CHECK_NE(Franc(0), *five1);
     
-    CHECK_EQ(d1, d1);
-    CHECK_EQ(d1, d2);
-    CHECK_EQ(d2, d2);
-    CHECK(d1==d2);
+    CHECK_EQ(*five1, *five1);
+    CHECK_EQ(*five2, *five2);
+    CHECK_EQ(*five1, *five2);
+    CHECK(*five1==*five2);
 }
 
 TEST_CASE("equals operator Franc and Dollar") {
-    Franc d1(5);
-    Dollar d2(5);
-    CHECK(d1 != d2);
+    std::unique_ptr<Money> fiveD = Money::dollar(5);
+    std::unique_ptr<Money> fiveF = Money::franc(5);
+    CHECK(fiveD != fiveF);
     CHECK(Franc(0)==Franc(0));
     CHECK(Franc(0)!=Franc(1));
     CHECK(Franc(0)!=Dollar(0));
